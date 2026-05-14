890b92e71b8f031efb2a2e996994840d87a79090
Dakota Seagraves <dakotaseagraves31@gmail.com>
2026-05-14 15:48:50 -0500

    Add getCharacter to characterServices module

--- src/components/services/characterServices.jsx
+++ src/components/services/characterServices.jsx
@@ -18,3 +18,12 @@ export const createCharacter = (payload) => {
         body: JSON.stringify(payload)
     }).then(res => res.json())
 }
+
+export const getCharacter = (id) => {
+    return fetch("http://localhost:8000/characters/" + id, {
+        headers: {
+            Authorization: "Token " + JSON.parse(localStorage.getItem('adventurerscodex_token')).token,
+            "Content-Type": "application/json"
+        }
+    }).then(res => res.json())
+}
